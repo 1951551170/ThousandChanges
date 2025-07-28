7fe3ee428c7ac249bbdf129d38cb1ac7372b884c
petrece <1951551170@qq.com>
2025-07-29 00:39:19 +0800

    引入springAi依赖

---
+++ src/main/java/com/petrece/ai_assistant/invoke/SpringAiAiInvoke.java
@@ -0,0 +1,24 @@
+package com.petrece.ai_assistant.invoke;
+
+import jakarta.annotation.Resource;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/*@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是Go学长"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}*/
